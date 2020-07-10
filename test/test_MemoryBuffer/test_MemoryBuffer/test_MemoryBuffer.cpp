d464727dad6513de9dd8d1f5635aa2b0bf09f1e8
Ryu <ryujt658@hanmail.net>
2020-07-10 11:26:28 +0900

    MemoryBuffer 테스트 추가

---
+++ test/test_MemoryBuffer/test_MemoryBuffer/test_MemoryBuffer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+#include <ryulib/MemoryBuffer.hpp>
+
+int main()
+{
+    MemoryBuffer memoryBuffer;
+
+    int buffer_size = 123;
+    void* buffer = malloc(buffer_size);
+
+    srand(time(0));
+
+    while (true) {
+        int data_size = (rand() % 1024) + 128;
+        memoryBuffer.write(buffer, buffer_size);
+        char* src = (char*) memoryBuffer.read(data_size);
+        if (src != nullptr) {
+            int count = 0;
+            for (int i = 0; i < data_size; i++) count = count + *src;
+            printf("data_size: %8d, count: %d \r", data_size, count);
+            free(src);
+        }
+    }
+}
