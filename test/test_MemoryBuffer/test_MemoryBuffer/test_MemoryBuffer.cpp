12bc23aa24527b01489d0e690805c2201c38385f
Ryu <ryujt658@hanmail.net>
2020-07-18 14:35:39 +0900

    action 연습으로 의미없는 커밋

--- test/test_MemoryBuffer/test_MemoryBuffer/test_MemoryBuffer.cpp
+++ test/test_MemoryBuffer/test_MemoryBuffer/test_MemoryBuffer.cpp
@@ -7,17 +7,20 @@ int main()
     MemoryBuffer memoryBuffer;
 
     int buffer_size = 123;
-    void* buffer = malloc(buffer_size);
+    void *buffer = malloc(buffer_size);
 
     srand(time(0));
 
-    while (true) {
+    while (true)
+    {
         int data_size = (rand() % 1024) + 128;
         memoryBuffer.write(buffer, buffer_size);
-        char* src = (char*) memoryBuffer.read(data_size);
-        if (src != nullptr) {
+        char *src = (char *)memoryBuffer.read(data_size);
+        if (src != nullptr)
+        {
             int count = 0;
-            for (int i = 0; i < data_size; i++) count = count + *src;
+            for (int i = 0; i < data_size; i++)
+                count = count + *src;
             printf("data_size: %8d, count: %d \r", data_size, count);
             free(src);
         }
