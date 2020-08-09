5ead76dba5dbdf73abbc6b254b74c8ecd9bc48bb
Ryu <ryujt658@hanmail.net>
2020-08-09 16:44:17 +0900

    WASAPI 관련 모듈 추가

---
+++ include/WASAPI/AudioCapture.hpp
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <WASAPI/SystemAudioCapture.hpp>
+#include <WASAPI/AudioCapture.hpp>
+#include <ryulib/base.hpp>
+#include <ryulib/debug_tools.hpp>
+#include <ryulib/AudioIO.hpp>
+#include <ryulib/ThreadQueue.hpp>
+
+typedef struct AudioCaptureOption {
+	int  mic_device_id = -1;
+	bool use_system_audio = false;
+	int channels = 1;
+	int sample_rate = 44100;
+	int sample_size = 4;
+	int frames = 1024;
+	int get_frame_size() { return channels * sample_size * frames;  }
+};
+
+class AudioCapture {
+public:
+	AudioCapture()
+	{
+		Audio::init();
+	}
+
+	~AudioCapture()
+	{
+		if (mic_ != nullptr) delete mic_;
+		if (silent_audio_ != nullptr) delete silent_audio_;
+		if (audio_data_ != nullptr) delete audio_data_;		
+	}
+
+	bool start(AudioCaptureOption option)
+	{
+		if (mic_ != nullptr) stop();
+
+		option_ = option;
+
+		mic_ = new AudioInput(option.channels, option.sample_rate, option.sample_size, option.frames);
+		mic_->setOnError([&](const void* sender, int error_code) {
+			do_error(mic_, error_code);
+		});
+		mic_->setOnData([&](const void* obj, const void* buffer, int buffer_size) {
+			on_data(mic_, buffer, buffer_size);
+		});
+
+		if (silent_audio_ != nullptr) delete silent_audio_;
+		silent_audio_ = new Memory(option.get_frame_size());
+
+		if (audio_data_ != nullptr) delete audio_data_;
+		audio_data_ = new Memory(option.get_frame_size());
+
+		system_audio_.start(option.channels, option.sample_rate, option.sample_size, option.frames);
+
+		return mic_->open(option.mic_device_id) == 0;
+	}
+
+	void stop()
+	{
+		on_data_ = nullptr;
+		on_error_ = nullptr;
+
+		system_audio_.stop();
+
+		if (mic_ == nullptr) return;
+
+		mic_->close();
+		delete mic_;
+		mic_ = nullptr;
+	}
+
+	float getMicVolume() { return volume_mic_; }
+	float getSystemVolume() { return volume_system_; }
+
+	void setMicMute(bool value) { is_mic_muted_ = value; }
+	void setSystemMute(bool value) { is_mic_muted_ = value; }
+
+	void setMicVolume(float value) { volume_mic_ = value; }
+	void setSystemVolume(float value) { volume_system_ = value; }
+
+	void setOnData(DataEvent event) { on_data_ = event; }
+	void setOnError(IntegerEvent event) { on_error_ = event; }
+
+private:
+	AudioInput* mic_ = nullptr;
+	SystemAudioCapture system_audio_;
+
+	// 충분한 크기의 묵음 처리용 데이터
+	Memory* silent_audio_ = nullptr;
+	Memory* audio_data_ = nullptr;
+
+	AudioCaptureOption option_;
+
+	bool is_mic_muted_ = false;
+	bool is_system_muted_ = false;
+	float volume_mic_ = 1.0;
+	float volume_system_ = 1.0;
+
+	DataEvent on_data_ = nullptr;
+	IntegerEvent on_error_ = nullptr;
+
+	void do_error(AudioInput* sender, int error_code)
+	{
+		if (on_error_ != nullptr) on_error_(this, error_code);
+	}
+
+	void on_data(AudioInput* sender, const void* buffer, int buffer_size)
+	{
+		void* mic_audio = (void*) buffer;
+
+		Memory* system_audio = system_audio_.getAudioData();
+		if (option_.use_system_audio == true) {
+			if (is_system_muted_ || (system_audio == nullptr)) {
+				system_audio = silent_audio_;
+			}
+		} else {
+			system_audio = silent_audio_;
+		}
+
+		if (is_mic_muted_) {
+			mic_audio = silent_audio_->getData();
+		};
+
+		float* mic = (float*) mic_audio;
+		float* system = (float*) system_audio->getData();
+		float* dst = (float*) audio_data_->getData();
+		for (int i = 0; i < (buffer_size / sizeof(float)); i++) {
+			*dst = *mic * volume_mic_ + *system * volume_system_;
+			mic++;
+			system++;
+			dst++;
+		}
+
+		if (system_audio != silent_audio_) delete system_audio;
+
+		if (on_data_ != nullptr) on_data_(this, audio_data_->getData(), audio_data_->getSize());
+	}
+};
---
+++ include/WASAPI/SystemAudioCapture.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <WASAPI/SystemAudio.hpp>
+#include <ffmpeg/AudioResampling.hpp>
+#include <ryulib/MemoryBuffer.hpp>
+#include <ryulib/ThreadQueue.hpp>
+#include <ryulib/SimpleThread.hpp>
+
+using namespace std;
+using namespace ryulib;
+
+class SystemAudioCapture {
+private:
+	const static int MAX_DELAY_COUNT = 2;
+
+public:
+	SystemAudioCapture()
+	{
+		system_audio_.setOnData([&](const void* sender, const void* data, int size) {
+			if (data == nullptr) return;
+			if (queue_out_.size() > MAX_DELAY_COUNT) return;
+
+			queue_in_.push(new Memory(data, size));
+		});
+
+		audio_resampling_.setOnData([&](const void* sender, const void* data, int size) {
+			if (data != nullptr) queue_out_.push(new Memory(data, size));
+		});
+
+		thread_ = new SimpleThread([&](SimpleThread* simpleThread) {
+			while (simpleThread->isTerminated() == false) {
+				Memory* data = queue_in_.pop();
+				if (data == NULL) {
+					simpleThread->sleep(1);
+					continue;
+				}
+
+				buffer_.write(data->getData(), data->getSize());
+				delete data;
+
+				void* src = buffer_.read(audio_resampling_.getSrcBufferSize());
+				if (src != nullptr) {
+					audio_resampling_.execute(src, audio_resampling_.getSrcBufferSize());
+					free(src);
+				}
+			}
+		});
+	}
+
+	~SystemAudioCapture()
+	{
+		thread_->terminateAndWait();
+		do_clear();
+	}
+
+	void start(int channels, int sample_rate, int sample_size, int frames)
+	{
+		do_clear();
+
+		system_audio_.open();
+		audio_resampling_.open(
+			system_audio_.getChannels(),
+			system_audio_.getSamples(),
+			system_audio_.getBitsPerSample() / 8,
+			channels, sample_rate, sample_size, frames
+		);
+	}
+
+	void stop()
+	{
+		system_audio_.close();
+		audio_resampling_.close();
+	}
+
+	Memory* getAudioData()
+	{
+		Memory* memory = queue_out_.pop();
+		//printf("SystemAudioCaptrue delay: %d \r", queue_out_.size());
+		if (memory == NULL) return nullptr;
+		return memory;
+	}
+
+private:
+	SystemAudio system_audio_;
+	MemoryBuffer buffer_;
+	AudioResampling audio_resampling_;
+	SimpleThread* thread_;
+	ThreadQueue<Memory*> queue_out_;
+	ThreadQueue<Memory*> queue_in_;
+
+	void do_clear()
+	{
+		buffer_.clear();
+
+		Memory* data = queue_in_.pop();
+		while (data != NULL) {
+			delete data;
+			data = queue_in_.pop();
+		}
+
+		data = queue_out_.pop();
+		while (data != NULL) {
+			delete data;
+			data = queue_out_.pop();
+		}
+	}
+};
