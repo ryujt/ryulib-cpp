de3ab5d9e28b7836c9cd1e0531d5810cf25208cf
Ryu <ryujt658@gmail.com>
2023-09-10 17:56:27 +0900

    Update SuspensionQueue.hpp

--- include/ryulib/SuspensionQueue.hpp
+++ include/ryulib/SuspensionQueue.hpp
@@ -10,75 +10,75 @@ template <typename T>
 class SuspensionQueue
 {
 private:
-	bool is_terminated_;
-	std::queue<T> queue_;
-	std::mutex mutex_;
-	std::condition_variable condition_;
+    bool is_terminated_;
+    std::queue<T> queue_;
+    std::mutex mutex_;
+    std::condition_variable condition_;
+
 public:
-	SuspensionQueue() :
-		is_terminated_(false)
-	{
+    SuspensionQueue() :
+        is_terminated_(false)
+    {
 
-	}
+    }
 
-	void push(T item)
-	{
-		std::unique_lock<std::mutex> lock(mutex_);
-		queue_.push(item);
-		lock.unlock();
+    void push(T item)
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        queue_.push(item);
+        lock.unlock();
 
-		condition_.notify_all();
-	}
+        condition_.notify_all();
+    }
 
-	T pop() 
-	{
-		std::unique_lock<std::mutex> lock(mutex_);
+    T pop()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
 
-		while (queue_.empty()) {
-			if (is_terminated_) return NULL;
-			condition_.wait(lock);
-		}
+        while (queue_.empty()) {
+            condition_.wait(lock);
+            if (is_terminated_) return NULL;
+        }
 
-		auto result = queue_.front();
-		queue_.pop();
-		return result;
-	}
+        auto result = queue_.front();
+        queue_.pop();
+        return result;
+    }
 
-	bool pop(T &item)
-	{
-		item = NULL;
+    bool pop(T& item)
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
 
-		std::unique_lock<std::mutex> lock(mutex_);
+        while (queue_.empty()) {
+            condition_.wait(lock);
+            if (is_terminated_) return false;
+        }
 
-		while (queue_.empty()) {
-			if (is_terminated_) return false;
-			condition_.wait(lock);
-		}
+        item = queue_.front();
+        queue_.pop();
+        return true;
+    }
 
-		item = queue_.front();
-		queue_.pop();
-		return item;
-	}
+    void terminate()
+    {
+        is_terminated_ = true;
+        condition_.notify_all();
+    }
 
-	void terminate() 
-	{
-		is_terminated_ = true;
-		condition_.notify_all();
-	}
 public:
-	bool is_terminated() { return is_terminated_;  }
-	
-	bool is_empty() 
-	{
-		std::unique_lock<std::mutex> lock(mutex_);
-		return queue_.empty();
-	}
+    bool is_terminated() { return is_terminated_; }
 
-	int size() 
-	{
-		std::unique_lock<std::mutex> lock(mutex_);
-		return queue_.size();
-	}
+    bool is_empty()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        return queue_.empty();
+    }
+
+    int size()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        return queue_.size();
+    }
 };
 
 #endif  // RYULIB_SUSPENSIONQUEUE_HPP
