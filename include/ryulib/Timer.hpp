497f0ad1b65a31601891bb572d56212a18fe46ce
Ryu <ryujt658@gmail.com>
2024-11-14 12:24:22 +0900

    Timer 클래스 개선: 마이크로초를 밀리초로 변환하고 CPU 사용 최적화

--- include/ryulib/Timer.hpp
+++ include/ryulib/Timer.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <chrono>
 #include <ryulib/base.hpp>
 #include <ryulib/Scheduler.hpp>
@@ -7,69 +6,63 @@
 using namespace std::chrono;
 
 namespace ryulib {
-	class Timer {
-	private:
+    class Timer {
+    public:
+        Timer() {
+            scheduler_.setOnRepeat([&]() {
+                auto new_tick = std::chrono::system_clock::now();
+                if (new_tick > old_tick_) {
+                    // microseconds를 milliseconds로 변환
+                    auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(new_tick - old_tick_);
+                    count_ms_ = count_ms_ + delta.count();
+                }
+                old_tick_ = new_tick;
 
-	public:
-		Timer()
-		{
-			scheduler_.setOnRepeat([&]() {
-				auto new_tick = std::chrono::system_clock::now();
-				if (new_tick > old_tick_) {
-					std::chrono::microseconds delta = std::chrono::duration_cast<std::chrono::microseconds>(new_tick - old_tick_);
-					count_ms_ = count_ms_ + delta.count();
-				}
-				old_tick_ = new_tick;
+                if (count_ms_ >= interval_) {
+                    if (on_timer_ != nullptr) on_timer_(this);
+                    count_ms_ = count_ms_ - interval_;
+                }
 
-				if (count_ms_ > interval_) {
-					if (on_timer_ != nullptr) on_timer_(this);
-					count_ms_ = count_ms_ - interval_;
-				}
+                // sleep 시간을 더 길게 설정하여 CPU 사용률 감소
+                scheduler_.sleep(10);  // 10ms 간격으로 체크
+                });
+        }
 
-				scheduler_.sleep(1);
-			});
-		}
+        void terminate() {
+            stop();
+            scheduler_.terminate();
+        }
 
-		void terminate()
-		{
-			stop();
-			scheduler_.terminate();
-		}
+        void terminateNow() {
+            stop();
+            scheduler_.terminateNow();
+        }
 
-		void terminateNow()
-		{
-			stop();
-			scheduler_.terminateNow();
-		}
+        void terminateAndWait() {
+            stop();
+            scheduler_.terminateAndWait();
+        }
 
-		void terminateAndWait()
-		{
-			stop();
-			scheduler_.terminateAndWait();
-		}
+        void start(int interval) {
+            interval_ = interval;
+            count_ms_ = 0;
+            old_tick_ = std::chrono::system_clock::now();
+            scheduler_.start();
+        }
 
-		void start(int interval)
-		{
-			interval_ = interval;
-			count_ms_ = 0;
-			old_tick_ = std::chrono::system_clock::now();
-			scheduler_.start();
-		}
+        void stop() {
+            scheduler_.stop();
+        }
 
-		void stop()
-		{
-			scheduler_.stop();
-		}
+        void setOnTimer(NotifyEvent event) {
+            on_timer_ = event;
+        }
 
-		void setOnTimer(NotifyEvent event) { on_timer_ = event; }
-
-	private:
-		Scheduler scheduler_;
-
-		int interval_ = 0;
-		int count_ms_ = 0;
-		system_clock::time_point old_tick_;
-
-		NotifyEvent on_timer_ = nullptr;
-	};
+    private:
+        Scheduler scheduler_;
+        int64_t interval_ = 0;        // int64_t로 변경하여 큰 간격도 처리
+        int64_t count_ms_ = 0;        // int64_t로 변경
+        system_clock::time_point old_tick_;
+        NotifyEvent on_timer_ = nullptr;
+    };
 }
